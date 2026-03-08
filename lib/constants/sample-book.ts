b61d1a838600e3ee0a3a0afbebdb7e71bb7780af
Creando-Conciencia <josra2001@gmail.com>
2026-03-08 15:16:29 -0600

    Update sample-book.ts

--- lib/constants/sample-book.ts
+++ lib/constants/sample-book.ts
@@ -11,6 +11,11 @@ export const sampleBook: Book = {
     title: "UJAT 2025: Transformando vidas y sociedades a través de la ciencia",
     isBookCover: true,
   },
+  {
+          type: "cover",
+          title: "..",
+          image: "/Libro_Hoja_Legal.jpeg",
+        } as PageContent,
   authors: {
     type: "authors",
     title: "Autores y Coautores",
