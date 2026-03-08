a5dc6e1c85a157f189e9eb578fda2df70e179020
Creando-Conciencia <josra2001@gmail.com>
2026-03-08 15:36:28 -0600

    Add files via upload

---
+++ components/book-reader/Hoja_Legal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const LegalPage: React.FC = () => {
+  return (
+    <div className="relative flex items-center justify-center min-h-screen bg-white">
+      <img
+        src="/Libro_Hoja_Legal.jpg"
+        alt="Hoja legal"
+        className="max-w-full max-h-full object-contain"
+      />
+    </div>
+  );
+};
